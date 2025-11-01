d44a2436ae1a0510074204b25439bca5b4814aff
andrearaujo-maker <andrearaujo0307@gmail.com>
2025-11-01 01:48:15 -0300

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,1 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100))
+    email = db.Column(db.String(120), unique=True)
+    senha = db.Column(db.String(100))
+    ativo = db.Column(db.Boolean, default=True)
+    chat_id = db.Column(db.String(100))
