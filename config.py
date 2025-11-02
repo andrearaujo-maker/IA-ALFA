0abb873bd2a57d947fd435f6214bb2abb5e25d24
andrearaujo-maker <andrearaujo0307@gmail.com>
2025-11-01 23:43:35 -0300

    Update and rename config.py to codigos.txt

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "chave-secreta-alfa")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///users.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "8126373920:AAEdRJ48gNqflX-M3kcihod4xegf314iup0")
